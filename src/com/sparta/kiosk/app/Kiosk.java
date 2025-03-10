c406f72bcd74da1ee951e3e2d51df92c4848d715
HWKIM <pottq577@naver.com>
2025-03-10 17:19:19 +0900

    Lv3 - Feat: 전체 순서 제어: Main -> Kiosk
    입력과 반복문 로직은 Kiosk 클래스의 start() 함수에서 관리

--- src/com/sparta/kiosk/app/Kiosk.java
+++ src/com/sparta/kiosk/app/Kiosk.java
@@ -2,8 +2,42 @@ package com.sparta.kiosk.app;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Kiosk {
 
   List<MenuItem> menuItems = new ArrayList<>();
+  Scanner scanner = new Scanner(System.in);
+
+  public Kiosk(List<MenuItem> menuItems) {
+    this.menuItems = menuItems;
+  }
+
+  public void start() {
+    while (true) {
+      System.out.println("[ SHAKESHACK MENU ]");
+      for (int i = 0; i < menuItems.size(); i++) {
+        System.out.println(
+            i + 1 + ". "
+                + menuItems.get(i).menuName + " | "
+                + "W " + menuItems.get(i).menuPrice
+                + " | " + menuItems.get(i).menuDesc);
+      }
+      System.out.println("0. 종료\n");
+
+      System.out.print("선택: ");
+      int userMenuChoice = scanner.nextInt();
+
+      if (userMenuChoice == 0) {
+        System.out.println("\n프로그램을 종료합니다.");
+        break;
+      } else {
+        System.out.println("선택한 메뉴: " +
+            menuItems.get(userMenuChoice - 1).menuName + ", " +
+            menuItems.get(userMenuChoice - 1).menuPrice + ", " +
+            menuItems.get(userMenuChoice - 1).menuDesc + "\n");
+      }
+    }
+
+  }
 }
