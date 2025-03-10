a0394cc32ed271b6a5f506bc62b87c423295fe2e
HWKIM <pottq577@naver.com>
2025-03-10 17:52:56 +0900

    Lv4 - Feat: 카테고리 출력

--- src/com/sparta/kiosk/app/Kiosk.java
+++ src/com/sparta/kiosk/app/Kiosk.java
@@ -1,38 +1,46 @@
 package com.sparta.kiosk.app;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Kiosk {
 
-  List<MenuItem> menuItems;
+  Menu menu;
+  List<Menu> menuList = new ArrayList<>();
   Scanner scanner = new Scanner(System.in);
 
+  public Kiosk(Menu menu) {
+    this.menu = menu;
+  }
+
   public void start() {
-    while (true) {
-      System.out.println("[ SHAKESHACK MENU ]");
-      for (int i = 0; i < menuItems.size(); i++) {
-        System.out.println(
-            i + 1 + ". "
-                + menuItems.get(i).menuName + " | "
-                + "W " + menuItems.get(i).menuPrice
-                + " | " + menuItems.get(i).menuDesc);
-      }
-      System.out.println("0. 종료\n");
 
-      System.out.print("선택: ");
-      int userMenuChoice = scanner.nextInt();
-
-      if (userMenuChoice == 0) {
-        System.out.println("\n프로그램을 종료합니다.");
-        break;
-      } else {
-        System.out.println("선택한 메뉴: " +
-            menuItems.get(userMenuChoice - 1).menuName + ", " +
-            menuItems.get(userMenuChoice - 1).menuPrice + ", " +
-            menuItems.get(userMenuChoice - 1).menuDesc + "\n");
-      }
+    System.out.println("[ MAIN MENU ]");
+    for (int i = 0; i < menu.getCategory().length; i++) {
+      System.out.println(i + 1 + ". " + menu.getCategory(i));
     }
+
+//    menu.printMenuItems();
+
+//    while (true) {
+//      System.out.println("[ SHAKESHACK MENU ]");
+//      menu.printMenuItems();
+//      System.out.println("0. 종료\n");
+//
+//      System.out.print("선택: ");
+//      int userMenuChoice = scanner.nextInt();
+//
+//      if (userMenuChoice == 0) {
+//        System.out.println("\n프로그램을 종료합니다.");
+//        break;
+//      } else {
+////        System.out.println("선택한 메뉴: " +
+////            menuItems.get(userMenuChoice - 1).menuName + ", " +
+////            menuItems.get(userMenuChoice - 1).menuPrice + ", " +
+////            menuItems.get(userMenuChoice - 1).menuDesc + "\n");
+//      }
+//    }
 
   }
 }
--- src/com/sparta/kiosk/app/Menu.java
+++ src/com/sparta/kiosk/app/Menu.java
@@ -1,13 +1,47 @@
 package com.sparta.kiosk.app;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Menu {
 
-  public String category;
-  public List<MenuItem> menuItems;
+  public String[] category = new String[3];
+  public List<MenuItem> menuItems = new ArrayList<>();
 
-  public Menu(List<MenuItem> menuItems) {
-    this.menuItems = menuItems;
+  public Menu(String[] category) {
+    this.category[0] = category[0];
+    this.category[1] = category[1];
+    this.category[2] = category[2];
+    addMenu();
+  }
+
+  public void addMenu(){
+    menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+    menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+    menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+    menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
+  }
+
+  public void printMenuItems(){
+    for (int i = 0; i < menuItems.size(); i++) {
+      System.out.println(
+          i + 1 + ". "
+              + menuItems.get(i).menuName + " | "
+              + "W " + menuItems.get(i).menuPrice
+              + " | " + menuItems.get(i).menuDesc);
+    }
+  }
+
+  public List<MenuItem> getMenuItems() {
+    return menuItems;
+  }
+
+  public String getCategory(int index) {
+    return category[index];
+  }
+
+  public String[] getCategory() {
+    return category;
   }
 }
