5f6b6bcb9f55c7ebef563a3f79a127abbf130e13
HWKIM <pottq577@naver.com>
2025-03-10 18:07:06 +0900

    Lv4 - Feat: 반복문 밖에서 단일 카테고리(현재는 햄버거)와 메뉴 선택 기능 구현

--- src/com/sparta/kiosk/app/Kiosk.java
+++ src/com/sparta/kiosk/app/Kiosk.java
@@ -15,30 +15,39 @@ public class Kiosk {
   }
 
   public void start() {
-
+//    카테고리 출력
     System.out.println("[ MAIN MENU ]");
     for (int i = 0; i < menu.getCategory().length; i++) {
       System.out.println(i + 1 + ". " + menu.getCategory(i));
     }
+    System.out.println("0. 종료");
+//    카테고리 선택
+    System.out.print("\n선택: ");
+    int userCategoryChoice = scanner.nextInt();
 
-//    menu.printMenuItems();
+//    선택된 카테고리 메뉴 출력
+    System.out.println();
+    System.out.println("[ " + menu.getCategory(userCategoryChoice - 1) + " MENU ]");
+    menu.printMenuItems();
+
+//    메뉴 선택
+    System.out.print("\n선택: ");
+    int userMenuChoice = scanner.nextInt();
+    System.out.println("선택한 메뉴: " +
+        menu.getMenuItems().get(userMenuChoice - 1).menuName + ", " +
+        menu.getMenuItems().get(userMenuChoice - 1).menuPrice + ", " +
+        menu.getMenuItems().get(userMenuChoice - 1).menuDesc
+    );
 
 //    while (true) {
-//      System.out.println("[ SHAKESHACK MENU ]");
-//      menu.printMenuItems();
-//      System.out.println("0. 종료\n");
-//
-//      System.out.print("선택: ");
-//      int userMenuChoice = scanner.nextInt();
-//
-//      if (userMenuChoice == 0) {
+//      if (userCategoryChoice == 0) {
 //        System.out.println("\n프로그램을 종료합니다.");
 //        break;
 //      } else {
-////        System.out.println("선택한 메뉴: " +
-////            menuItems.get(userMenuChoice - 1).menuName + ", " +
-////            menuItems.get(userMenuChoice - 1).menuPrice + ", " +
-////            menuItems.get(userMenuChoice - 1).menuDesc + "\n");
+//        System.out.println("선택한 메뉴: " +
+//            menuItems.get(userMenuChoice - 1).menuName + ", " +
+//            menuItems.get(userMenuChoice - 1).menuPrice + ", " +
+//            menuItems.get(userMenuChoice - 1).menuDesc + "\n");
 //      }
 //    }
 
