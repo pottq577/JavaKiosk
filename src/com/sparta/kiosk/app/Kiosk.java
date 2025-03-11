2585e424ee9a1d8d7cf20576c46ae088650d128a
HWKIM <pottq577@naver.com>
2025-03-11 09:58:59 +0900

    Lv4 완료 - Develop to Main

--- src/com/sparta/kiosk/app/Kiosk.java
+++ src/com/sparta/kiosk/app/Kiosk.java
@@ -1,43 +1,64 @@
 package com.sparta.kiosk.app;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Kiosk {
 
-  List<MenuItem> menuItems = new ArrayList<>();
+  Menu menu;
   Scanner scanner = new Scanner(System.in);
 
-  public Kiosk(List<MenuItem> menuItems) {
-    this.menuItems = menuItems;
+  public Kiosk(Menu menu) {
+    this.menu = menu;
   }
 
   public void start() {
     while (true) {
-      System.out.println("[ SHAKESHACK MENU ]");
-      for (int i = 0; i < menuItems.size(); i++) {
-        System.out.println(
-            i + 1 + ". "
-                + menuItems.get(i).menuName + " | "
-                + "W " + menuItems.get(i).menuPrice
-                + " | " + menuItems.get(i).menuDesc);
-      }
-      System.out.println("0. 종료\n");
-
-      System.out.print("선택: ");
-      int userMenuChoice = scanner.nextInt();
-
-      if (userMenuChoice == 0) {
+//      카테고리 선택
+      printCategory();
+      int userCategoryChoice = scanner.nextInt();
+      if (userCategoryChoice == 0) {
         System.out.println("\n프로그램을 종료합니다.");
         break;
-      } else {
-        System.out.println("선택한 메뉴: " +
-            menuItems.get(userMenuChoice - 1).menuName + ", " +
-            menuItems.get(userMenuChoice - 1).menuPrice + ", " +
-            menuItems.get(userMenuChoice - 1).menuDesc + "\n");
       }
-    }
 
+//      카테고리 내 메뉴 선택
+      printMenu(userCategoryChoice);
+      int userMenuChoice = scanner.nextInt();
+      if (userMenuChoice == 0) {
+        System.out.println("\n메인 메뉴로 돌아갑니다.");
+        System.out.println();
+        continue;
+      }
+
+//      선택한 메뉴 출력
+      printUserMenu(userMenuChoice, menu.getCategory(userCategoryChoice - 1));
+    }
+  }
+
+  public void printCategory() {
+    System.out.println("[ MAIN MENU ]");
+    for (int i = 0; i < menu.getCategory().length; i++) {
+      System.out.println(i + 1 + ". " + menu.getCategory(i));
+    }
+    System.out.println("0. 종료");
+    System.out.print("\n선택: ");
+  }
+
+  public void printMenu(int userCategoryChoice) {
+    String selectedCategory = menu.getCategory(userCategoryChoice - 1);
+    System.out.println("\n[ " + selectedCategory + " MENU ]");
+    menu.printMenuItems(selectedCategory);
+    System.out.println("0. 뒤로가기");
+
+    System.out.print("\n선택: ");
+  }
+
+  public void printUserMenu(int userMenuChoice, String category) {
+    MenuItem selectedMenu = menu.getMenuItems(category).get(userMenuChoice - 1);
+    System.out.println("선택한 메뉴: " +
+        selectedMenu.menuName + " | " +
+        "W " + selectedMenu.menuPrice + " | " +
+        selectedMenu.menuDesc + "\n"
+    );
   }
 }
