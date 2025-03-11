ffc5d44d7862dda02f2685c8b8511191f0c53c75
HWKIM <pottq577@naver.com>
2025-03-11 15:31:55 +0900

    Lv5 완성 - Develop to Main

--- src/com/sparta/kiosk/app/MenuItem.java
+++ src/com/sparta/kiosk/app/MenuItem.java
@@ -1,13 +1,25 @@
 package com.sparta.kiosk.app;
 
 public class MenuItem {
-  public String menuName;
-  public Double menuPrice;
-  public String menuDesc;
+  private final String menuName;
+  private final Double menuPrice;
+  private final String menuDesc;
 
   public MenuItem(String menuName, Double menuPrice, String menuDesc){
     this.menuName = menuName;
     this.menuPrice = menuPrice;
     this.menuDesc = menuDesc;
+  }
+
+  public String getMenuName() {
+    return menuName;
+  }
+
+  public Double getMenuPrice() {
+    return menuPrice;
+  }
+
+  public String getMenuDesc() {
+    return menuDesc;
   }
 }
